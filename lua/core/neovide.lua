fbd56a6283dc9cbf40d351f9636c426ff8065faf
radish <radish@192.168.1.11>
2024-03-16 11:40:10 +0800

    添加neovide字体调节功能

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,5 +1,5 @@
-local font = 'Hack Nerd Font Mono'
-local default_fontsize = 13
+local font = 'Firacode Nerd Font Mono'
+local default_fontsize = 11
 
 vim.o.guifont = font .. ':h' .. default_fontsize
 vim.g.neovide_floating_blur_amount_x = 1.5
@@ -11,6 +11,22 @@ vim.g.neovide_confirm_quit = true
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_cursor_unfocused_outline_width = 0.1
 vim.g.neovide_cursor_vfx_mode = 'pixiedust'
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_input_macos_alt_is_meta = true
 
 vim.opt.winblend = 35
 vim.opt.pumblend = 35
+
+local get_guifontsize = function ()
+  return string.split(vim.o.guifont, ':h')[2]
+end
+
+vim.keymap.set('n', '<M-->', function ()
+  vim.o.guifont = font .. ':h' .. get_guifontsize() - 1
+  print(get_guifontsize())
+end, {noremap = true})
+
+vim.keymap.set('n', '<M-=>', function ()
+  vim.o.guifont = font .. ':h' .. get_guifontsize() + 1
+  print(get_guifontsize())
+end, {noremap = true})
