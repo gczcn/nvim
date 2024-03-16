598b7bb277c43fb3b0d4e310f9145a8b21cfa75e
radish <radish@192.168.1.11>
2024-03-16 11:42:32 +0800

    修改map为vim.keymap.set

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -2,13 +2,13 @@ return{
   'CRAG666/code_runner.nvim',
   event = fileopened,
   config = function ()
-    map('n', '<leader>r', '<cmd>RunCode<CR>', { noremap = true, silent = false })
-    map('n', '<leader>rf', '<cmd>RunFile<CR>', { noremap = true, silent = false })
-    map('n', '<leader>rft', '<cmd>RunFile tab<CR>', { noremap = true, silent = false })
-    map('n', '<leader>rp', '<cmd>RunProject<CR>', { noremap = true, silent = false })
-    map('n', '<leader>rc', '<cmd>RunClose<CR>', { noremap = true, silent = false })
-    map('n', '<leader>crf', '<CMD>CRFiletype<CR>', { noremap = true, silent = false })
-    map('n', '<leader>crp', '<cmd>CRProjects<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>r', '<cmd>RunCode<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>rf', '<cmd>RunFile<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>rft', '<cmd>RunFile tab<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>rp', '<cmd>RunProject<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>rc', '<cmd>RunClose<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>crf', '<CMD>CRFiletype<CR>', { noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>crp', '<cmd>CRProjects<CR>', { noremap = true, silent = false })
     require('code_runner').setup({
       filetype = {
         markdown = function ()
